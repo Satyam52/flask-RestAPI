951b5b8c31e01781c7f9ecccae67346b134500fc
Satyam52 <satyam0350@gmail.com>
2020-05-25 11:57:04 +0530

    (chore):Added test branch

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda item: item["name"] == name, items), None)
+        return {"item": item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda item: item["name"] == name, items), None):
+            return {"message": "An item with name {} already exists".format(name)}, 400
+        data = request.get_json()
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}, 200
+
+
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
+app.run(port=5000, debug=True)
